854d799604aa417082e003adbf97a2b24aca7bdd
Decsu-k <k-rozhin@mail.ru>
2022-04-03 17:18:20 +0300

    + folders

---
+++ MVVM_1.2_part/MathFuncs/MathFuncs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MVVM_1._2_part
+{
+    static class MathFuncs
+    {
+        public static int GetSumOf(int a, int b) => a + b;
+    }
+}
